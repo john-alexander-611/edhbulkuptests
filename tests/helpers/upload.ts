2d5a3d67e196c71e38b78fa38da705323927fb50
John Alexander <not.john.alexander@gmail.com>
2026-09-15 15:01:53 -0700

    adding wait to try to fix commander page tests

--- tests/helpers/upload.ts
+++ tests/helpers/upload.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 import fs from 'fs';
 
 export async function uploadCollection(page: Page, filename: string) {
@@ -12,6 +12,10 @@ export async function openCommanderPage(page: Page, commanderName: string) {
   await page.getByTestId('exclude-partners-checkbox').uncheck();
   await page.getByTestId('only-owned-checkbox').uncheck();
   await page.getByTestId('sample-collection-button').click();
+
+  // wait for confirmation the collection actually finished loading before searching
+  await expect(page.getByTestId('collection-hint-message')).toBeVisible();
+
   const searchBar = page.getByTestId('commander-search-input');
   await searchBar.fill(commanderName);
   await page.getByTestId('search-commanders-button').click();
